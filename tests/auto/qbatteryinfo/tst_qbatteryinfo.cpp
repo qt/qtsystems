cbb4d1f9c0812f5d6fabb162523ee89514c09972
Xizhi Zhu <xizhi.zhu@nokia.com>
2011-07-04 11:09:38 +0300

    Improve tests for QBatteryInfo.

--- tests/auto/qbatteryinfo/tst_qbatteryinfo.cpp
+++ tests/auto/qbatteryinfo/tst_qbatteryinfo.cpp
@@ -51,38 +51,66 @@ class tst_QBatteryInfo : public QObject
 private slots:
     void tst_capacity();
     void tst_flow();
+    void tst_invalid();
 };
 
 void tst_QBatteryInfo::tst_capacity()
 {
     QBatteryInfo batteryInfo;
-    int max = batteryInfo.maximumCapacity(0);
-    int remaining = batteryInfo.remainingCapacity(0);
 
-    QVERIFY(max == -1 || remaining == -1 || remaining <= max);
+    int count = batteryInfo.batteryCount();
+    for (int i = 0; i < count; ++i) {
+        int max = batteryInfo.maximumCapacity(i);
+        int remaining = batteryInfo.remainingCapacity(i);
+
+        QVERIFY(max == -1 || remaining == -1 || remaining <= max);
+    }
 }
 
 void tst_QBatteryInfo::tst_flow()
 {
     QBatteryInfo batteryInfo;
-    QBatteryInfo::ChargingState chargingState = batteryInfo.chargingState(0);
 
-    switch (chargingState) {
-    case QBatteryInfo::Charging:
-        QVERIFY(batteryInfo.currentFlow(0) < 0);
-        QVERIFY(batteryInfo.remainingChargingTime(0) > 0);
-        break;
-    case QBatteryInfo::NotCharging:
-        QVERIFY(batteryInfo.currentFlow(0) == 0);
-        QVERIFY(batteryInfo.remainingChargingTime(0) == 0);
-        break;
-    case QBatteryInfo::Discharging:
-        QVERIFY(batteryInfo.currentFlow(0) > 0);
-        QVERIFY(batteryInfo.remainingChargingTime(0) == 0);
-        break;
-    case QBatteryInfo::UnknownChargingState:
-        break;
+    int count = batteryInfo.batteryCount();
+    for (int i = 0; i < count; ++i) {
+        QBatteryInfo::ChargingState chargingState = batteryInfo.chargingState(i);
+        switch (chargingState) {
+        case QBatteryInfo::Charging:
+            QVERIFY(batteryInfo.currentFlow(i) < 0);
+            QVERIFY(batteryInfo.remainingChargingTime(i) > 0);
+            break;
+        case QBatteryInfo::NotCharging:
+            QVERIFY(batteryInfo.currentFlow(i) == 0);
+            QVERIFY(batteryInfo.remainingChargingTime(i) == 0);
+            break;
+        case QBatteryInfo::Discharging:
+            QVERIFY(batteryInfo.currentFlow(i) > 0);
+            QVERIFY(batteryInfo.remainingChargingTime(i) == 0);
+            break;
+        case QBatteryInfo::UnknownChargingState:
+            break;
+        }
     }
+}
+
+void tst_QBatteryInfo::tst_invalid()
+{
+    QBatteryInfo batteryInfo;
+
+    QVERIFY(batteryInfo.currentFlow(-1) == 0);
+    QVERIFY(batteryInfo.maximumCapacity(-1) == -1);
+    QVERIFY(batteryInfo.remainingCapacity(-1) == -1);
+    QVERIFY(batteryInfo.remainingChargingTime(-1) == -1);
+    QVERIFY(batteryInfo.voltage(-1) == -1);
+    QVERIFY(batteryInfo.chargingState(-1) == QBatteryInfo::UnknownChargingState);
+
+    int count = batteryInfo.batteryCount();
+    QVERIFY(batteryInfo.currentFlow(count) == 0);
+    QVERIFY(batteryInfo.maximumCapacity(count) == -1);
+    QVERIFY(batteryInfo.remainingCapacity(count) == -1);
+    QVERIFY(batteryInfo.remainingChargingTime(count) == -1);
+    QVERIFY(batteryInfo.voltage(count) == -1);
+    QVERIFY(batteryInfo.chargingState(count) == QBatteryInfo::UnknownChargingState);
 }
 
 QTEST_MAIN(tst_QBatteryInfo)
